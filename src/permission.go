1ae543503ab4a9e9d8ac1e44a95dbbfcd9375a88
Thiago Zilli <thiago.zilli@gmail.com>
2017-02-14 09:45:04 -0200

    Fix permission

--- src/permission.go
+++ src/permission.go
@@ -8,11 +8,11 @@ import (
 
 //Flags for biwise
 const (
-	FLAGREAD   int = 1  // 0001
-	FLAGWRITE  int = 2  // 0010
-	FLAGUPDATE int = 4  // 0100
-	FLAGDELETE int = 8  // 1000
-	FLAGALL    int = 16 // 10000
+	FLAGREAD   int = 1  // 000001
+	FLAGWRITE  int = 2  // 000010
+	FLAGUPDATE int = 4  // 000100
+	FLAGDELETE int = 8  // 001000
+	FLAGALL    int = 16 // 010000
 	GOD        int = 32 // 100000
 )
 
@@ -66,9 +66,11 @@ func checkFlags(route string, perms int) bool {
 }
 
 func checkUserPermisson(action string, endpoint string) {
-	//recupera do contexto o user_id
+	//recover the user_id from context
+
 	// find no mongodb { user: user_id, endpoint: endpoint }
-	//return do mongo as actions
+
+	//return mongodb the actions
 	var actions = map[string]int{
 		//"GET":  1,
 		//"POST": 2,
@@ -77,15 +79,15 @@ func checkUserPermisson(action string, endpoint string) {
 		//"ALL": 16,
 	}
 
+	//intereate each permission and sum
 	var perms = 0
 	for k, v := range actions {
-		//if strings.Contains(action, k) {
 		fmt.Printf("key[%s] value[%d]\n", k, v)
 		perms |= v
 		fmt.Printf("permission  = %d\n", perms)
-		//}
 	}
 
+	//check if return something and the flags are ok
 	if perms > 0 && checkFlags(action, perms) {
 		log.Printf("Authorized")
 	} else {
